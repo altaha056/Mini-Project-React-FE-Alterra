46b905dcdcf53c4c9b34625bba8f0e2ba79edef6
Altaha <altahabinsyafriadi@gmail.com>
2021-10-06 15:17:22 +0700

    rabu v4

--- src/graphql/mutation.js
+++ src/graphql/mutation.js
@@ -9,7 +9,7 @@ export const InsertPost = gql`
 `;
 
 export const UpdatePost = gql`
-  mutation MyMutation($id: Int = 10, $title: String!) {
+  mutation updatePost($id: Int = 10, $title: String!) {
     update_antonio_post_by_pk(
       pk_columns: { id: $id }
       _set: { title: $title }
@@ -20,9 +20,49 @@ export const UpdatePost = gql`
 `;
 
 export const DeletePost = gql`
-  mutation MyMutation($id: Int!) {
+  mutation aeletePost($id: Int!) {
     delete_antonio_post_by_pk(id: $id) {
       id
     }
   }
 `;
+
+export const AddLike = gql`
+  mutation addLike($object: antonio_likes_insert_input!) {
+    insert_antonio_likes_one(object: $object) {
+      id
+    }
+  }
+`;
+export const DeleteLike = gql`
+  mutation deleteLike($post_id: Int!, $user_id: Int!) {
+    delete_antonio_likes(
+      where: { post_id: { _eq: $post_id }, user_id: { _eq: $user_id } }
+    ) {
+      affected_rows
+    }
+  }
+`;
+export const AddComment = gql`
+  mutation addComment($object: antonio_comment_insert_input!) {
+    insert_antonio_comment_one(object: $object) {
+      id
+    }
+  }
+`;
+export const SignUp=gql``;
+
+
+export const SignIn = gql``;
+
+
+
+
+
+
+
+
+
+
+
+
